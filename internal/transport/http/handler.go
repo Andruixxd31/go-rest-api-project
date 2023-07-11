9083e6e3265ec8ba0d4996815d693c5e54ce3a85
adnruixxd31 <andruix93@gmail.com>
2023-07-11 17:22:36 -0600

    feat: graceful shutdown

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,8 +1,13 @@
 package http
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -39,8 +44,20 @@ func (handler *Handler) mapRoutes(){
 }
 
 func (handler *Handler) Serve() error {
-    if err := handler.Server.ListenAndServe(); err != nil {
-        return err
-    }
+    go func() {
+        if err := handler.Server.ListenAndServe(); err != nil {
+            log.Println(err.Error())
+        }
+    }()
+
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt)
+    <-c
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+    handler.Server.Shutdown(ctx)
+
+    log.Println("shut down gracefully")
     return nil
 }
