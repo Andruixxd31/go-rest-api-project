fa387d8e4815958edddc20e631886ac4bdb14a83
Andruixxd31 <andruix93@gmail.com>
2023-07-10 23:16:56 -0600

    feat: setup db packaging and ping

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,26 @@
 // manages instantiations of components and layers and joinig de layers together
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+    "github.com/andruixxd31/go-rest-api/internal/db"
+)
 
 // Responsible for instantiation and startup of application
 // Creates layers and plumbs them together
 func Run() error {
 
     fmt.Println("Starting up app")
+    db, err := db.NewDatabase()
+    if err != nil {
+        fmt.Errorf("Failed to connect to db")
+        return err
+    }
+    if err := db.Healthcheck(context.Background()); err != nil {
+        return err
+    }
     return nil
 }
 
